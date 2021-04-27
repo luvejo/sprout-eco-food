3a9c195ebdfdd5d87bca2d01173978066fbc1a26
Luis Velásquez <luvejo.dev@gmail.com>
2021-04-26 21:34:44 -0500

    refactor: automatically import base components

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,25 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+// Automatically import base components.
+// prettier-ignore
+const requireComponent = require.context('./components', true, /Base[A-Z]\w+\.(vue|js)$/)
+
+// prettier-ignore
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName = baseComponentConfig.name || (
+    fileName
+      .replace(/^.+\//, '')
+      .replace(/\.\w+$/, '')
+  )
+  app.component(baseComponentName, baseComponentConfig)
+})
+
+app
+  .use(store)
+  .use(router)
+  .mount('#app')
