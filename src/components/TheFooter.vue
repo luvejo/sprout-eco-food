38c058e08244cf436bf3127cdb23c9a5f64212b5
Luis Velásquez <luvejo.dev@gmail.com>
2021-05-11 16:25:39 -0500

    feat: add footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,49 +1,69 @@
 <template>
-  <footer class="footer">
-    <div class="footer__copyright-col">
-      <div class="footer__logo">SPROUT</div>
-      <p class="footer__company">
-        Eco Food Market <br />
-        SPROUT Good Ltd.
-      </p>
-      <p class="footer__copyright">2021 © All rights reserved</p>
-    </div>
+  <div class="footer-wrapper">
+    <div class="content-max-width">
+      <footer class="footer">
+        <div class="footer__title newsletters-title">Get our newsletters</div>
+        <base-input-text
+          placeholder="Your email"
+          class="newsletters-input"
+          :icon="{ name: 'check', background: true }"
+        />
 
-    <div class="footer__link-col">
-      <div class="footer__col-title">Company</div>
-      <router-link class="footer__link" to="/about">About</router-link>
-      <router-link class="footer__link" to="/store">Store</router-link>
-      <router-link class="footer__link" to="/faq">FAQ</router-link>
-    </div>
+        <div class="footer__link-col">
+          <div class="footer__title">Company</div>
+          <div class="footer__link-list">
+            <router-link class="footer__link" to="/about">About</router-link>
+            <router-link class="footer__link" to="/store">Store</router-link>
+            <router-link class="footer__link" to="/faq">FAQ</router-link>
+          </div>
+        </div>
 
-    <div class="footer__link-col">
-      <div class="footer__col-title">Service</div>
-      <router-link class="footer__link" to="/delivery">Delivery</router-link>
-      <router-link class="footer__link" to="/payment">Payment</router-link>
-      <router-link class="footer__link" to="/contacts">Contacts</router-link>
-    </div>
+        <div class="footer__link-col">
+          <div class="footer__title">Service</div>
+          <div class="footer__link-list">
+            <router-link class="footer__link" to="/delivery"
+              >Delivery</router-link
+            >
+            <router-link class="footer__link" to="/payment"
+              >Payment</router-link
+            >
+            <router-link class="footer__link" to="/contacts"
+              >Contacts</router-link
+            >
+          </div>
+        </div>
 
-    <div class="footer__link-col">
-      <div class="footer__col-title">Follow us</div>
-      <router-link class="footer__link" to="/instagram">Instagram</router-link>
-      <router-link class="footer__link" to="/facebook">Facebook</router-link>
-      <router-link class="footer__link" to="/twitter">Twitter</router-link>
-    </div>
+        <div class="footer__link-col">
+          <div class="footer__title">Follow us</div>
+          <div class="footer__link-list">
+            <router-link class="footer__link" to="/instagram"
+              >Instagram</router-link
+            >
+            <router-link class="footer__link" to="/facebook"
+              >Facebook</router-link
+            >
+            <router-link class="footer__link" to="/twitter"
+              >Twitter</router-link
+            >
+          </div>
+        </div>
 
-    <div class="footer__subscribe-col">
-      <div class="footer__col-title">Follow us</div>
-      <base-text-input
-        placeholder="Your email"
-        :icon="{ name: '', black: true }"
-      />
-      <router-link class="footer__link footer__link--legal" to="/tyc"
-        >Terms & Conditions</router-link
-      >
-      <router-link class="footer__link footer__link--legal" to="/tyc"
-        >Privacy Policy</router-link
-      >
+        <div class="footer__logo">SPROUT</div>
+        <p class="footer__company">
+          <span class="footer__brand">Eco Food Market</span> SPROUT Good Ltd.
+        </p>
+        <div class="footer__link-list legal-link-list">
+          <router-link class="footer__link legal-link" to="/tyc"
+            >Terms & Conditions</router-link
+          >
+          <router-link class="footer__link legal-link" to="/tyc"
+            >Privacy Policy</router-link
+          >
+        </div>
+        <p class="footer__copyright">2021 © All rights reserved</p>
+      </footer>
     </div>
-  </footer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -54,4 +74,172 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '@/assets/css/vars' as vars;
+
+.footer-wrapper {
+  background-color: vars.$dark-cream;
+}
+
+.footer {
+  padding: 56px 44px;
+  display: grid;
+  margin-top: 56px;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(5, auto);
+
+  &__logo {
+    font-family: 'Gilroy';
+    font-weight: vars.$bold;
+    font-size: 20px;
+  }
+
+  &__link-col {
+    padding: 24px 0;
+    border-bottom: 2px solid vars.$light-gray;
+  }
+
+  &__title {
+    font-size: 1.6rem;
+    font-family: Gilroy;
+    font-weight: vars.$bold;
+    margin-bottom: 16px;
+  }
+
+  &__link:not(:last-of-type) {
+    margin-right: 28px;
+  }
+
+  &__logo {
+    margin-top: 44px;
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  &__company {
+    text-align: center;
+    margin-bottom: 16px;
+  }
+
+  &__copyright {
+    text-align: center;
+    color: vars.$gray;
+  }
+}
+
+.newsletters-title {
+  font-size: 2rem;
+  font-weight: vars.$semibold;
+}
+
+.newsletters-input {
+  width: 100%;
+  margin-bottom: 44px;
+}
+
+.legal-link-list {
+  margin-bottom: 16px;
+  text-align: center;
+  text-decoration: underline;
+}
+
+/*----------------------------------------------
+  MEDIA QUERIES
+-----------------------------------------------*/
+@media screen and (min-width: 880px) {
+  .footer {
+    grid-template-columns: 1fr repeat(3, 120px) 1fr;
+    grid-template-rows: 1fr;
+    padding: 56px;
+
+    &__logo {
+      margin: 0px;
+      grid-column: 1;
+      grid-row: 1;
+      align-self: start;
+      text-align: left;
+      font-size: 24px;
+    }
+
+    &__company {
+      grid-column: 1;
+      grid-row: 1;
+      align-self: center;
+      text-align: left;
+      margin-bottom: 16px;
+    }
+
+    &__brand {
+      display: block;
+    }
+
+    &__copyright {
+      grid-column: 1;
+      grid-row: 1;
+      align-self: end;
+      text-align: left;
+      color: vars.$gray;
+    }
+
+    &__link-col {
+      align-self: start;
+      padding: 0px;
+      border: none;
+    }
+
+    &__link-list {
+      display: flex;
+      flex-direction: column;
+    }
+
+    &__link {
+      margin-bottom: 12px;
+    }
+  }
+
+  .newsletters-title {
+    grid-column: -2;
+    grid-row: 1;
+    align-self: start;
+    font-size: inherit;
+    font-weight: vars.$semibold;
+  }
+
+  .newsletters-input {
+    grid-column: -2;
+    grid-row: 1;
+    align-self: center;
+    width: 100%;
+    margin-bottom: 0px;
+  }
+
+  .legal-link-list {
+    grid-column: -2;
+    grid-row: 1;
+    align-self: end;
+    margin-bottom: 0px;
+    display: flex;
+    justify-self: start;
+    flex-direction: row;
+  }
+
+  .legal-link {
+    text-align: left;
+    margin: 0px;
+    font-size: 1.4rem;
+    color: vars.$gray;
+    text-decoration: underline;
+    text-decoration-color: vars.$gray;
+
+    &:not(:last-of-type) {
+      margin-right: 16px;
+    }
+  }
+}
+
+@media screen and (min-width: 1000px) {
+  .footer {
+    grid-template-columns: 1fr repeat(3, 180px) 1fr;
+  }
+}
+</style>
